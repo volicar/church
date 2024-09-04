9dbac591108e1ce0e72b973d602ff63d12b70fc9
Vinícius Olicar <olicar311@gmail.com>
2024-09-04 18:12:27 -0300

    feat: add bannercourosel to the home

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import { Divide } from "lucide-react";
 import Image from "next/image";
 import Header from "@/components/Header"
+import BannerCarousel from "@/components/BannerCarousel";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <BannerCarousel />
     </div>
   );
 }
---
+++ src/components/BannerCarousel.js
@@ -0,0 +1,65 @@
+'use client'
+
+import { useState } from "react";
+import { Button } from "@/components/ui/Button";
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+
+
+const slides = [
+  {
+    desktop: "https://placehold.co/1920x650",
+    mobile: "https://placehold.co/360x380",
+  },
+  {
+    desktop: "https://placehold.co/1920x650",
+    mobile: "https://placehold.co/360x380",
+  },
+  {
+    desktop: "https://placehold.co/1920x650",
+    mobile: "https://placehold.co/360x380",
+  }
+];
+
+export default function BannerCarousel() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => setCurrentSlide((prevSlide) => (prevSlide === slides.length - 1 ? 0 : prevSlide + 1));
+  const prevSlide = () => setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 1 : prevSlide - 1));
+
+  return (
+    <div className="relative w-full h-[650px] overflow-hidden">
+
+      <div
+        className="flex transition-transform duration-500 ease-in-out"
+        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+      >
+        {slides.map((slide, index) => (
+          <picture key={index} className="w-full h-[650px] flex-shrink-0">
+            <source media="(max-width: 768px)" srcSet={slide.mobile} />
+            <img
+              src={slide.desktop}
+              alt={`Slide ${index + 1}`}
+              className="w-full h-full object-cover"
+            />
+          </picture>
+        ))}
+      </div>
+
+      <Button
+        variant="outline"
+        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white/70"
+        onClick={prevSlide}
+      >
+        <ChevronLeftIcon className="h-6 w-6 text-gray-800" />
+      </Button>
+      <Button
+        variant="outline"
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white/70"
+        onClick={nextSlide}
+      >
+        <ChevronRightIcon className="h-6 w-6 text-gray-800" />
+      </Button>
+    </div>
+  );
+}
+
